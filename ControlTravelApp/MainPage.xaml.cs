7f0ec18991b2ee860d875f3117507603f87a1dd8
SolveNaN <dagoberto6993@gmail.com>
2024-10-20 22:14:21 -0500

    first commit

---
+++ ControlTravelApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ControlTravelApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControlTravelWeb/ControlTravelWeb/Connection.cs
@@ -0,0 +1,8 @@
+﻿namespace ControWellApp
+{
+    public class Connection
+    {
+        public string server = "https://controltravelservertesting-aqcmajcwa8c9b9e3.canadacentral-01.azurewebsites.net/";
+        public string blobserverfiles = "https://generalfilesblazorapp.blob.core.windows.net/travel";
+    }
+}
